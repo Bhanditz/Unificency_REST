b1fa4e76d814a7249f1cbf073446d35c6ea48b8e
romue404 <robert.mueller1990@googlemail.com>
2017-01-10 19:26:45 +0100

    added documentation

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Unificency API",  "version": "0.1.0",  "description": "Dokumentation der Unificency RESTful API",  "title": "Unificency API",  "url": "romue404.pythonanywhere.com/api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-01-10T17:31:20.774Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
